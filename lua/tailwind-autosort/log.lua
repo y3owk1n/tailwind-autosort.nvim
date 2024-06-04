2019d723ebc9cb08bc6aa1b2f420d027214b98af
y3owk1n <wongyeowkin@gmail.com>
2024-06-04 16:02:34 +0800

    feat: fix notify typo

--- lua/tailwind-autosort/log.lua
+++ lua/tailwind-autosort/log.lua
@@ -2,22 +2,22 @@ local M = {}
 
 ---@param message string
 M.info = function(message)
-	return M.notify(message, vim.log.levels.INFO)
+	return vim.notify(message, vim.log.levels.INFO)
 end
 
 ---@param message string
 M.warn = function(message)
-	return M.notify(message, vim.log.levels.WARN)
+	return vim.notify(message, vim.log.levels.WARN)
 end
 
 ---@param message string
 M.error = function(message)
-	return M.notify(message, vim.log.levels.ERROR)
+	return vim.notify(message, vim.log.levels.ERROR)
 end
 
 ---@param message string
 M.debug = function(message)
-	return M.notify(message, vim.log.levels.DEBUG)
+	return vim.notify(message, vim.log.levels.DEBUG)
 end
 
 return M
