c29c8cfdc62b62837cc226a4b71cdde622d9b2ad
y3owk1n <wongyeowkin@gmail.com>
2024-06-08 12:13:14 +0800

    feat: add ternary support

--- queries/tsx/cn.scm
+++ queries/tsx/cn.scm
@@ -3,5 +3,8 @@
   (#any-of? @function_name "cn" "clsx" "twMerge")
   (arguments
     (string (string_fragment) @cn_class)
+    (ternary_expression
+      consequence: (string (string_fragment) @tailwind)
+      alternative: (string (string_fragment) @tailwind))
   )
 )
