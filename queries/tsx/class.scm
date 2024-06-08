d1447f0fabad0182aa94a41cd260158074b7d29e
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2024-06-08 12:13:46 +0800

    feat: add ternary support (#22)

--- queries/tsx/class.scm
+++ queries/tsx/class.scm
@@ -2,10 +2,13 @@
   (property_identifier) @_attribute_name
   (#any-of? @_attribute_name "class" "className")
   [
-   (string
-     (string_fragment) @tailwind)
-   (jsx_expression
-     (template_string
-       (string_fragment) @tailwind))
-   ])
-
+    (string
+      (string_fragment) @tailwind)
+    (jsx_expression
+      (template_string
+        (string_fragment) @tailwind))
+    (jsx_expression
+    (ternary_expression
+      consequence: (string (string_fragment) @tailwind)
+      alternative: (string (string_fragment) @tailwind)))
+  ])
