9f8dbad269d545e61d562cdb2bc747f1f6f2e38e
y3owk1n <wongyeowkin@gmail.com>
2024-06-08 11:55:29 +0800

    feat: remove tailwind root caching
    Just use the lsp information instead

--- lua/tailwind-autosort/cache.lua
+++ lua/tailwind-autosort/cache.lua
@@ -1,17 +1,14 @@
 local M = {}
 
 ---@class TailwindAutoSort.Cache
----@field tw_root_dir string|nil|false
 ---@field prettier_root_dir string|nil|false
 ---@field has_tw_prettier_plugin boolean|nil
 M.cache = {
-	tw_root_dir = nil,
 	prettier_root_dir = nil,
 	has_tw_prettier_plugin = nil,
 }
 
 M.reset_cache = function()
-	M.cache.tw_root_dir = nil
 	M.cache.prettier_root_dir = nil
 	M.cache.has_tw_prettier_plugin = nil
 end
--- lua/tailwind-autosort/file.lua
+++ lua/tailwind-autosort/file.lua
@@ -1,31 +1,9 @@
 local M = {}
 
 local cache = require("tailwind-autosort.cache")
-local log = require("tailwind-autosort.log")
 
 M.find_root = function(ctx, file_patterns)
 	return vim.fs.find(file_patterns, { path = ctx.filename, upward = true })[1]
-end
-
-M.find_root_dir = function(ctx, file_patterns)
-	local found_file =
-		vim.fs.find(file_patterns, { path = ctx.filename, upward = true })[1]
-	if found_file then
-		return vim.fn.fnamemodify(found_file, ":h")
-	else
-		return nil
-	end
-end
-
-M.is_in_root_directory = function(ctx, file_patterns, cache_root_dir)
-	local root_dir = M.find_root_dir(ctx, file_patterns)
-
-	-- Compare the current directory with the root directory
-	if cache_root_dir == root_dir then
-		return true
-	else
-		return false
-	end
 end
 
 ---@param file string
@@ -49,36 +27,6 @@ M.find_text_in_file = function(search_text, path)
 
 	-- Return the count
 	return count or 0 -- If the conversion fails, return 0
-end
-
-M.set_tw_root = function()
-	local tw_file_pattern = {
-		"tailwind.config.ts",
-		"tailwind.config.js",
-	}
-
-	local ctx = {}
-	ctx.filename = vim.fn.expand("%:p")
-
-	if
-		cache.cache.tw_root_dir == nil
-		or M.is_in_root_directory(
-				ctx,
-				tw_file_pattern,
-				cache.cache.tw_root_dir
-			)
-			== false
-	then
-		local tw_root = M.find_root_dir(ctx, tw_file_pattern)
-
-		if not tw_root then
-			cache.cache.tw_root_dir = false
-			return
-		end
-
-		cache.cache.tw_root_dir = tw_root
-		return
-	end
 end
 
 M.set_prettier_root = function()
