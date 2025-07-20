a2aceee023edfd9d6e701e8a9f386ae571e80306
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-07-20 14:40:31 +0800

    fix(docs): switch doc gen from `pandocvim` to `vimcats`

--- lua/tailwind-autosort/config.lua
+++ lua/tailwind-autosort/config.lua
@@ -1,13 +1,30 @@
+---@mod tailwind-autosort.nvim.config Configurations
+---@brief [[
+---
+---Example Configuration:
+---
+--->
+---{
+---	enable_autocmd = true,
+---	notify_line_changed = true,
+---}
+---<
+---
+---@brief ]]
+
 local M = {}
 
+---@type TailwindAutoSort.Config
 M.config = {}
 
+---@private
 ---@type TailwindAutoSort.Config
 local defaults = {
 	enable_autocmd = true,
 	notify_line_changed = true,
 }
 
+---@private
 ---@param user_config? TailwindAutoSort.Config
 M.setup = function(user_config)
 	M.config = vim.tbl_deep_extend("force", defaults, user_config or {})
